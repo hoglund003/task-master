9ea91e94da7dbf259b91fd2ce40a1952d2eee540
Kasper Høglund <kasper@rubynor.no>
2022-09-26 16:44:15 +0200

    task_records#create

--- app/controllers/task_records_controller.rb
+++ app/controllers/task_records_controller.rb
@@ -1,6 +1,19 @@
 class TaskRecordsController < ApplicationController
   def new
+    @task_record = TaskRecord.new
     @users = User.all.map{|user| [user.name, user.id]}
     @tasks = Task.all.map{|task| [task.name, task.id]}
+  end
+
+  def create
+    @task_record = TaskRecord.new(user_id: params[:user], task_id: params[:task])
+
+    if @task_record.save
+      flash[:notice] = "The task was saved!"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "The task was not saved!"
+      render "new"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :task_records, only: [:new]
+  resources :task_records, only: [:new, :create]
 
   root "task_records#new"
 end
