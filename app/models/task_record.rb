41d660ba87e640bbad6891f69e1b2a995dda095b
Kasper Høglund <kasper@rubynor.no>
2022-09-27 11:56:19 +0200

    point functions to models added

--- app/models/task_record.rb
+++ app/models/task_record.rb
@@ -1,4 +1,8 @@
 class TaskRecord < ApplicationRecord
   belongs_to :user
   belongs_to :task
+
+  def points
+    Task.find(task_id).points.to_i
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: {minimum:2, maximum:30}
   has_many :task_records
+
+  def points
+    task_records.sum {|record| record.points}
+  end
 end
