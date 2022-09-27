f1e6f61d921590bbc60a483d0250ff4a6aaa5c32
Kasper Høglund <kasper@rubynor.no>
2022-09-27 10:41:23 +0200

    default value of 'done_at'

--- db/migrate/20220926133643_create_task_records.rb
+++ db/migrate/20220926133643_create_task_records.rb
@@ -3,7 +3,7 @@ class CreateTaskRecords < ActiveRecord::Migration[7.0]
     create_table :task_records do |t|
       t.integer :user_id
       t.integer :task_id
-      t.datetime :done_at
+      t.datetime :done_at, :default => Time.now
       
       t.timestamps
     end
