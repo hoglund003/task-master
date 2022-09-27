d752b6292f5410869829b2bbed4a4c6afd166ae8
Kasper Høglund <kasper@rubynor.no>
2022-09-27 14:34:58 +0200

    Task.user added

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,4 +7,10 @@ class Task < ApplicationRecord
     # Is the task done today?
     TaskRecord.where(task_id: self.id, done_at: (Time.now.midnight)..(Time.now.midnight + 1.day)).any?
   end
+
+  def user
+    # Returns the User who did the task last
+    record = TaskRecord.where(task_id: self.id, done_at: (Time.now.midnight)..(Time.now.midnight + 1.day)).last
+    User.find(record.user_id) unless User.find(record.user_id).nil?
+  end
 end
