8b587c91f4d3f8f53e877252d2470db75b8d8465
Kasper Høglund <kasper@rubynor.no>
2022-09-28 13:43:13 +0200

    users#show added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :task_records, only: [:index, :new, :create, :show]
-  resources :users, only: [:index]
+  resources :users, only: [:index, :show]
 
   root "task_records#new"
 end
