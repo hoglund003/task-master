eee23f1887b4ad66485b461c30e6b1caa2e23413
Kasper Høglund <kasper@rubynor.no>
2022-10-03 11:16:52 +0200

    can display multipe avatars

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,9 +8,9 @@ class Task < ApplicationRecord
     TaskRecord.where(task_id: self.id, done_at: (Time.now.midnight)..(Time.now.midnight + 1.day)).any?
   end
 
-  def user
-    # Returns the User who did the task last
-    record = TaskRecord.where(task_id: self.id, done_at: (Time.now.midnight)..(Time.now.midnight + 1.day)).last
-    User.find(record.user_id) unless User.find(record.user_id).nil?
+  def users
+    # Returns the 4 Users who last did the task today
+    task_records = TaskRecord.where(task_id: self.id, done_at: (Time.now.midnight)..(Time.now.midnight + 1.day))
+    User.where(id: task_records.map{|task_record|task_record.user_id}).last(4)
   end
 end
