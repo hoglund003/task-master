e5d6a6ff1d374afa13ba1ea1d37915b033095b65
Kasper Høglund <kasper@rubynor.no>
2022-10-04 15:34:10 +0200

    buttons in _week updates date in url

--- app/javascript/custom/app.js
+++ app/javascript/custom/app.js
@@ -1,6 +1,7 @@
 const navbar = document.querySelector("nav.navbar");
 const menuButton = document.querySelector(".menu-button");
 const navBottomATags = document.querySelectorAll("nav.navbar .bottom a");
+const weekButtons = document.querySelectorAll(".wday");
 
 var navbarExpanded = false;
 
@@ -26,5 +27,17 @@ document.addEventListener('click', (e) => {
     navBottomATags.forEach(child=>{
       child.style.display = "none";
     });
+  }
+});
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const parameter_date = urlParams.get('date');
+
+weekButtons.forEach(btn=>{
+  if (btn.value == parameter_date) {
+    btn.classList.add("active");
+  } else {
+    btn.classList.remove("active");
   }
 });
