7ef7a4c139eb3493b3e46bffbc1197ff0f75d7d6
Kasper Høglund <kasper@rubynor.no>
2022-10-05 11:45:24 +0200

    using Turbo to render form

--- app/controllers/task_records_controller.rb
+++ app/controllers/task_records_controller.rb
@@ -7,15 +7,22 @@ class TaskRecordsController < ApplicationController
     @task_record = TaskRecord.new
     @users = User.all.map{|user| [user.name, user.id]}
     @tasks = Task.all
-    @selected_date = params[:date]
+    @selected_date = params[:date] || Date.today
+
+    if turbo_frame_request?
+      render partial: "board", :locals => { tasks: @tasks, users: @users, selected_date: @selected_date }
+      return
+    end
   end
 
   def create
-    @task_record = TaskRecord.new(user_id: params[:user], task_id: params[:task], done_at: Time.now)
-
+    @task_record = TaskRecord.new(user_id: params[:user], task_id: params[:task], done_at: params[:selected_date])
+    
     if @task_record.save
+      @users = User.all.map{|user| [user.name, user.id]}
+      @tasks = Task.all
       flash[:notice] = "The task was saved!"
-      redirect_to root_path
+      render partial: "board", :locals => { tasks: @tasks, users: @users, selected_date: params[:selected_date], }
     else
       flash[:alert] = "The task was not saved!"
       redirect_to root_path
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,13 +3,13 @@ class Task < ApplicationRecord
   validates :points, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 10}
   has_many :task_records
 
-  def done?(selected_date=nil)
+  def done?(selected_date=Date.today.to_s)
     selected_date = selected_date.to_date
     # Is the task done today?
-    TaskRecord.where(task_id: self.id, done_at: (selected_date.midnight)..(selected_date.midnight + 1.day)).any? unless selected_date.nil?
+    TaskRecord.where(task_id: self.id, done_at: (selected_date.midnight)..(selected_date.midnight + 1.day)).any?
   end
 
-  def users(selected_date=nil)
+  def users(selected_date=Date.today.to_s)
     selected_date = selected_date.to_date
     # Returns the 4 Users who last did the task today
     task_records = TaskRecord.where(task_id: self.id, done_at: (selected_date.midnight)..(selected_date.midnight + 1.day))
