dc81f7f74fcbb3ba67d537a6f3506d24d2472e16
Kasper Høglund <kasper@rubynor.no>
2022-10-10 11:21:40 +0200

    Show only users with point on scoreboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.sort_by{|user| -user.points_at(params[:start_date], params[:end_date])}[0, 5] # Returs the 5 users with most points
+    @users = User.all.sort_by{|user| -user.points_at(params[:start_date], params[:end_date])}[0, 5].select{|user| user.points_at(params[:start_date], params[:end_date]) > 0} # Returs the 5 users with most points
     if turbo_frame_request?
       render partial: "users/scoreboard", locals: { users: @users }
       return
