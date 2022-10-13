9167937cbb5370fcd1d806f27893e857d1607233
Kasper Høglund <kasper@rubynor.no>
2022-10-13 13:16:49 +0200

    under_construction view for Stats

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/under_construction', :to => "under_construction#static_pages"
   resources :task_records, only: [:index, :new, :create, :show, :destroy]
   resources :users, only: [:index, :show] do 
     resources :task_records, only: [:index]
   end
-  resources :stats, only: [:index]
+  # resources :stats, only: [:index]
+  get "stats", :to => "static_pages#under_construction"
 
   root "task_records#new"
 end
