eb2d2ce21a4ae9dcf585de6357101be40c38b558
Kasper Høglund <kasper@rubynor.no>
2022-10-17 15:23:17 +0200

    Added slack-client gem

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module TaskMaster
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    Slack.configure do |config|
+      config.token = ENV['SLACK_API_TOKEN']
+    end
   end
 end
