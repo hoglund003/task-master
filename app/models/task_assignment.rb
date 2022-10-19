a33b0a21d86b4f0718711810cbb1eaba3893d2b5
Kasper Høglund <kasper@rubynor.no>
2022-10-19 16:36:34 +0200

    function for generating slack interaction

--- app/models/task_assignment.rb
+++ app/models/task_assignment.rb
@@ -1,4 +1,50 @@
 class TaskAssignment < ApplicationRecord
   belongs_to :user
   belongs_to :task
+
+  def slack_block
+    [
+      {
+        "type": "section",
+        "text": {
+          "type": "plain_text",
+          "text": "You have been assigned a task:",
+          "emoji": true
+        }
+      },
+      {
+        "type": "header",
+        "text": {
+          "type": "plain_text",
+          "text": "#{task.name}"
+        }
+      },
+      {
+        "type": "actions",
+        "block_id": "#{id}",
+        "elements": [
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "emoji": true,
+              "text": "I can do it!"
+            },
+            "style": "primary",
+            "value": "accepted"
+          },
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "emoji": true,
+              "text": "I can't do it"
+            },
+            "style": "danger",
+            "value": "denied"
+          }
+        ]
+      }
+    ]
+  end
 end
