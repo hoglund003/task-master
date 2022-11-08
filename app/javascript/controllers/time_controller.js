291a4b63f1969499089b8162614ca0e5eca2999f
Kasper Høglund <kasper@rubynor.no>
2022-11-08 09:50:35 +0100

    redo auto-refresh

--- app/javascript/controllers/time_controller.js
+++ app/javascript/controllers/time_controller.js
@@ -3,12 +3,12 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   connect() {
+    // check time every minute
     setInterval(() => {
-      var date = new Date()
-      if (date.getHours() == 0) {
-        console.log("reload")
+      var date = new Date();
+      if (date.getHours() == 0 && date.getMinutes() == 1) { // reload if time == 00:01
         window.location.reload()
       }
-    }, 1000 * 60 * 60)
+    }, 1000 * 60)
   }
 }
