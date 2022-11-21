e5803a0585539ddfb5f3e37c779a678a8f908c67
kasper_hoglund <kasper@rubynor.com>
2022-11-21 08:50:38 +0100

    instruction migration

--- db/migrate/20221121074111_create_instructions.rb
+++ db/migrate/20221121074111_create_instructions.rb
@@ -1,6 +1,8 @@
 class CreateInstructions < ActiveRecord::Migration[7.0]
   def change
     create_table :instructions do |t|
+      t.integer :task_id
+      t.text :body
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_133554) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_074111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "changes", force: :cascade do |t|
     t.integer "task_record_id"
     t.string "action"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "instructions", force: :cascade do |t|
+    t.integer "task_id"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
