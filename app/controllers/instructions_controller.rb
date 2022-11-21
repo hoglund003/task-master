ab37699023c3ee5cf59ad9020dccf13a5877a770
kasper_hoglund <kasper@rubynor.com>
2022-11-21 10:25:15 +0100

    instructions#show added

--- app/controllers/instructions_controller.rb
+++ app/controllers/instructions_controller.rb
@@ -2,4 +2,8 @@ class InstructionsController < ApplicationController
   def index
     @instructions = Instruction.all
   end
+
+  def show
+    @instruction = Instruction.find(params[:id])
+  end
 end
