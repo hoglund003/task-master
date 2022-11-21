0a919d160258bf35a8adbcd5decbb430ca04213a
kasper_hoglund <kasper@rubynor.com>
2022-11-21 08:53:50 +0100

    task to instruction relationship

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ApplicationRecord
   validates :name, :uniqueness => true
   validates :points, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 10}
   has_many :task_records
+  has_one :instruction
 
   def done?(selected_date=Date.today.to_s)
     selected_date = selected_date.to_date
