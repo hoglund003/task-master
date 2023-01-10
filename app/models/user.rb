cf689d56f6ebfe26a5a73b97e7a6fa5624a2a6fa
kasper_hoglund <kasper@rubynor.com>
2023-01-10 13:20:01 +0100

    fix date bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
 
   def points_last_month
     today = Date.today
-    start_date = Date.new(today.year, today.prev_month.mon)
+    start_date = Date.new(today.prev_month.year, today.prev_month.mon)
     end_date = Date.new(today.year, today.month, 1)
     points_at(start_date, end_date)
   end
@@ -27,8 +27,8 @@ class User < ApplicationRecord
 
   def points_last_week
     today = Date.today
-    monday_last_week = (today - 1.week).beginning_of_week
-    sunday_last_week = (today - 1.week).end_of_week
+    monday_last_week = today.prev_week
+    sunday_last_week = monday_last_week.end_of_week
     start_date = monday_last_week
     end_date = sunday_last_week
     points_at(start_date, end_date)
